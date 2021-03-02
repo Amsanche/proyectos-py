d9565f38417c97ac6027e654cc5cb6797470ef27
Alberto Sánchez <Alberto.Sanchez@PC-ASanchez.bimsar.local>
2021-03-02 11:16:02 -0600

    Hola mundo en python

---
+++ Intro.py
@@ -0,0 +1,7 @@
+nombre = ""
+nombre=input("ingrese su nombre")
+print("Hola {}".format (nombre) )
+edad=0
+edad=int(input("ingrese su edad"))
+print("tu edad es {}".format (edad) )
+print(type(edad))
